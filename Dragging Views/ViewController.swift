35fe6a0290d2d1fa3d7334351e8520e92e4e8ddb
monschiTee <ramonasrbecky@Ramonas-MacBook-Pro.local>
2017-04-01 13:09:16 +0200

    Initial Commit

---
+++ Dragging Views/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dragging Views
+//
+//  Created by Ramona Srbecky on 01.04.17.
+//  Copyright © 2017 Ramona Srbecky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
